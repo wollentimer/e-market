76a7a186791da8230becba9ad1d5f7c773761b0b
Николай Рузвельт <NikolayKusakin@yandex.by>
2022-07-20 23:33:28 +0300

    Add repositories and interfaces

---
+++ backend/src/Market.DataAccess.Database/Repositories/GameRepository.cs
@@ -0,0 +1,13 @@
+﻿using Market.DataAccess.Database.Context;
+using Market.DataAccess.Database.Entities;
+using Market.DataAccess.Database.Interfaces;
+
+namespace Market.DataAccess.Database.Repositories
+{
+    public class GameRepository : GenericRepository<Game>, IGameRepository
+    {
+        public GameRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ backend/src/Market.DataAccess.Database/Repositories/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Market.DataAccess.Database.Context;
+using Market.DataAccess.Database.Entities;
+using Market.DataAccess.Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Market.DataAccess.Database.Repositories
+{
+	public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+		where TEntity : BaseEntity
+	{
+		protected ApplicationDbContext Context { get; }
+		protected DbSet<TEntity> EntityDbSet { get; }
+        public virtual IQueryable<TEntity> Query => EntityDbSet.AsQueryable();
+
+		public GenericRepository(ApplicationDbContext context)
+        {
+            Context = context;
+            EntityDbSet = context.Set<TEntity>();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+			return await EntityDbSet.AsNoTracking().ToListAsync();
+		}
+
+        public virtual async Task<TEntity> GetByIdAsync(int id)
+        {
+			return (await EntityDbSet.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id))!;
+		}
+
+		public virtual async Task InsertAsync(TEntity entity)
+		{
+			await EntityDbSet.AddAsync(entity);
+			await Context.SaveChangesAsync();
+
+			DetachAsync(entity);
+		}
+
+		public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+		{
+			EntityDbSet.Update(entity);
+			
+			await Context.SaveChangesAsync();
+			
+			DetachAsync(entity);
+			
+			return entity;
+		}
+
+		public virtual async Task DeleteAsync(int id)
+		{
+			TEntity entity = await Query.FirstAsync(x => x.Id == id);
+            if (entity == null) throw new ArgumentNullException(nameof(entity));
+
+			await DeleteAsync(entity);
+		}
+
+		public virtual async Task DeleteAsync(TEntity entity)
+		{
+			EntityDbSet.Remove(entity);
+			await Context.SaveChangesAsync();
+		}
+
+		protected void DetachAsync<T>(T entity)
+			where T : class
+		{
+			Context.Entry(entity).State = EntityState.Detached;
+		}
+	}
+}
