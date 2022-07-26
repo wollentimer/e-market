101b6664d1d6578907a49626fbbb9d32f7437a08
Николай Рузвельт <NikolayKusakin@yandex.by>
2022-07-26 16:15:59 +0300

    Create interfaces and services

---
+++ backend/src/Market.Business.Services/Interfaces/IGameService.cs
@@ -0,0 +1,6 @@
+﻿namespace Market.Business.Services.Interfaces
+{
+    public interface IGameService
+    {
+    }
+}
---
+++ backend/src/Market.Business.Services/Services/UserService.cs
@@ -0,0 +1,8 @@
+﻿using Market.Business.Services.Interfaces;
+
+namespace Market.Business.Services.Services
+{
+    public class UserService : IUserService
+    {
+    }
+}
