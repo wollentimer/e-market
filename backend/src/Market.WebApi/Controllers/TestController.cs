a2a3d8a918419af0fd3da0f5d7e76a643a026b79
Николай Рузвельт <NikolayKusakin@yandex.by>
2022-07-27 18:35:24 +0300

    Add test controller

---
+++ backend/src/Market.WebApi/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Market.Business.Services.Interfaces;
+using Market.WebApi.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        private readonly IGameService _gameService;
+        private readonly IMapper _mapper;
+
+        public TestController(IMapper mapper, IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet("Get")]
+        public async Task<IEnumerable<GameViewModel>> Get()
+        {
+            var result = _mapper.Map<IEnumerable<GameViewModel>>(await _gameService.GetAllAsync());
+            return result;
+        }
+    }
+}
