6c6c4724a70d7827ae23c18f61d29c578fdab505
Николай Рузвельт <NikolayKusakin@yandex.by>
2022-07-27 18:31:04 +0300

    Update interfaces and services

--- backend/src/Market.Business.Services/Interfaces/IGameService.cs
+++ backend/src/Market.Business.Services/Interfaces/IGameService.cs
@@ -1,6 +1,9 @@
-﻿namespace Market.Business.Services.Interfaces
+﻿using Market.Business.Entities.Database;
+
+namespace Market.Business.Services.Interfaces
 {
-    public interface IGameService
+    public interface IGameService : IBaseService<GameDto>
     {
+
     }
 }
--- backend/src/Market.Business.Services/Services/UserService.cs
+++ backend/src/Market.Business.Services/Services/UserService.cs
@@ -1,8 +1,33 @@
-﻿using Market.Business.Services.Interfaces;
+﻿using Market.Business.Entities.Database;
+using Market.Business.Services.Interfaces;
 
 namespace Market.Business.Services.Services
 {
     public class UserService : IUserService
     {
+        public Task<UserDto> CreateAsync(UserDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<UserDto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDto> UpdateAsync(UserDto entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
